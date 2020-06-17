03a7aa9503e135b422183f7b96cfa1af4f2cf90b
ciru <pciruelos@yahoo.com>
2020-06-17 11:37:59 -0300

    jama

--- src/database.js
+++ src/database.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
-const { JAMA_BASIC_HOST,JAMA_BASIC_DATABASE} = process.env;
-const MONGODB_URI=`mongodb://${JAMA_BASIC_HOST}/${JAMA_BASIC_DATABASE}`;
+// const { JAMA_BASIC_HOST,JAMA_BASIC_DATABASE} = process.env;
+// const MONGODB_URI=`mongodb://${JAMA_BASIC_HOST}/${JAMA_BASIC_DATABASE}`;
+
+const MONGODB_URI = process.env.MONGODB_URI;
 
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
